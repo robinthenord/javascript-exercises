3fbd8fde42d01866b11cc52ec421420fd9410a41
Robin <168934937+robinthenord@users.noreply.github.com>
2024-05-19 19:09:08 +0200

    complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  let str= '';
+  if (num < 0) return 'ERROR';
+  for (i = 0; i < num; i++) {
+    str = string + str;
+  }
+  return str;
 };
 
 // Do not edit below this line
