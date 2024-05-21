206054df6beb2b4cf8bcde1bda15548cc3423cf7
Robin <168934937+robinthenord@users.noreply.github.com>
2024-05-21 19:00:31 +0200

    complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  let low = 0;
+  let high = 0;
+  if (
+    num1 < 0 ||
+    num2 < 0 ||
+    typeof num1 !== "number" ||
+    typeof num2 !== "number"
+  ) {
+    return "ERROR";
+  }
 
+  if (num1 < num2) {
+    low = num1;
+    high = num2;
+  } else if (num1 > num2) {
+    low = num2;
+    high = num1;
+  }
+
+  let sum = 0;
+  for (low; low <= high; low++) {
+    sum += low;
+  }
+  return sum;
 };
 
 // Do not edit below this line
