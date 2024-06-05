9ddf817909ea696d011e109b4400ed6b7b22c513
Robin <168934937+robinthenord@users.noreply.github.com>
2024-06-05 15:40:35 +0200

    complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((a, b) => a + b, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((a, b) => a * b, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let sum = a;
+	for (i = 1; i < b; i++) {
+    sum *= a;
+  }
+  return sum;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let sum = 1;
+  if (num === 0) {
+    return 1;
+  }
+	for (i = 1; i <= num; i++) {
+    sum *= i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
