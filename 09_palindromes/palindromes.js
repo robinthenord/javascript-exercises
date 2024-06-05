00d6384a8b64e37557d36e4f4c7f242e29fa4857
Robin <168934937+robinthenord@users.noreply.github.com>
2024-06-05 16:07:26 +0200

    complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  let filtered = str
+      .toLowerCase()
+      .split('')
+      .filter(item => alphanumerical.includes(item))
+      .join('');
+  let reversed = filtered
+      .split('')
+      .reverse()
+      .join('');
+  return reversed === filtered;
 };
 
 // Do not edit below this line
